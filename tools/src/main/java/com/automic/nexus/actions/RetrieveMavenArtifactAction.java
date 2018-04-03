9adfa12d187ff62db20c8497fc8e89488bcd88f7
KaranAttli <31465153+KaranAttli@users.noreply.github.com>
2018-04-03 12:33:47 +0530

    PMARA-8060<PMARA-8195>:code review comments incorporated

--- tools/src/main/java/com/automic/nexus/actions/RetrieveMavenArtifactAction.java
+++ tools/src/main/java/com/automic/nexus/actions/RetrieveMavenArtifactAction.java
@@ -17,7 +17,7 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 /**
- * This action is used to upload an artifact to Nexus RAW repository.
+ * This action is used to retrieve an artifact from Nexus MAVEN repository.
  * 
  * @author Karanvir Attli
  *
@@ -36,31 +36,28 @@ public class RetrieveMavenArtifactAction extends AbstractHttpAction {
 	private static final String CLASSIFIER = "classifier";
 	private static final String EXTENSION = "extension";
 	
-	
-	
-
 	private String repository;
-	private String group;
+	private String groupid;
 	private String artifactid;
 	private String baseversion;
-	private String sha1;
-	private String md5;
+	private String filesha1;
+	private String filemd5;
 	private String targetFolder;
 	private String fileName;
-	private String classifier;
-	private String extension;
+	private String fileclassifier;
+	private String fileExtension;
 
 
 
 	public RetrieveMavenArtifactAction() {
-		addOption(RAW_REPO, true, "Repository");
-		addOption(GROUP, false, "Group name");
-		addOption(ARTIFACT_ID, false, "artifact id of the component");
+		addOption(RAW_REPO, true, "Repository Name");
+		addOption(GROUP, false, "Group ID");
+		addOption(ARTIFACT_ID, false, "Artifact ID");
 		addOption(BASE_VERSION, false, "Base Version");
 		addOption(SHA1, false, "SHA 1");
 		addOption(MD5, false, "MD5");
 		addOption(TARGET_FOLDER, false, "Target Folder");
-		addOption(FILE_NAME, true, "Name of the file");
+		addOption(FILE_NAME, true, "File Name");
 		addOption(CLASSIFIER, false, "classifier");
 		addOption(EXTENSION, false, "extension");
 	}
@@ -74,17 +71,14 @@ public class RetrieveMavenArtifactAction extends AbstractHttpAction {
 
 		repository = getOptionValue(RAW_REPO);
 		NexusValidator.checkNotEmpty(repository, "Repository");
-
-		group = getOptionValue(GROUP);
+		groupid = getOptionValue(GROUP);
 		artifactid = getOptionValue(ARTIFACT_ID);
 		baseversion = getOptionValue(BASE_VERSION);
-		sha1 = getOptionValue(SHA1);
-		md5 = getOptionValue(MD5);
+		filesha1 = getOptionValue(SHA1);
+		filemd5 = getOptionValue(MD5);
 		targetFolder = getOptionValue(TARGET_FOLDER);
-		
-		classifier = getOptionValue(CLASSIFIER);
-		extension = getOptionValue(EXTENSION);
-		
+		fileclassifier = getOptionValue(CLASSIFIER);
+		fileExtension = getOptionValue(EXTENSION);
 		fileName = getOptionValue(FILE_NAME);
 		NexusValidator.checkNotEmpty(fileName, "File Name");
 
@@ -104,8 +98,8 @@ public class RetrieveMavenArtifactAction extends AbstractHttpAction {
 		webResource = webResource.path("service").path("rest").path("beta").path("search").path("assets")
 				.path("download").queryParam("repository", repository);
 
-		if (CommonUtil.checkNotEmpty(group)) {
-			webResource = webResource.queryParam("maven.groupId", group);
+		if (CommonUtil.checkNotEmpty(groupid)) {
+			webResource = webResource.queryParam("maven.groupId", groupid);
 		}
 		if (CommonUtil.checkNotEmpty(artifactid)) {
 			webResource = webResource.queryParam("maven.artifactId", artifactid);
@@ -114,28 +108,24 @@ public class RetrieveMavenArtifactAction extends AbstractHttpAction {
 			webResource = webResource.queryParam("maven.baseVersion", baseversion);
 		}
 
-		if (CommonUtil.checkNotEmpty(md5)) {
-			webResource = webResource.queryParam("md5", md5);
+		if (CommonUtil.checkNotEmpty(filemd5)) {
+			webResource = webResource.queryParam("md5", filemd5);
 		}
 
-		if (CommonUtil.checkNotEmpty(sha1)) {
-			webResource = webResource.queryParam("sha1", sha1);
+		if (CommonUtil.checkNotEmpty(filesha1)) {
+			webResource = webResource.queryParam("sha1", filesha1);
 		}
 
-		if (CommonUtil.checkNotEmpty(extension)) {
-			webResource = webResource.queryParam("maven.extension", extension);
+		if (CommonUtil.checkNotEmpty(fileExtension)) {
+			webResource = webResource.queryParam("maven.extension", fileExtension);
 		}
 		
-		if (CommonUtil.checkNotEmpty(classifier)) {
-			webResource = webResource.queryParam("maven.classifier", classifier);
+		if (CommonUtil.checkNotEmpty(fileclassifier)) {
+			webResource = webResource.queryParam("maven.classifier", fileclassifier);
 		}
-		
-		
 		
 		ConsoleWriter.writeln("Calling url " + webResource.getURI());
-
 		response = webResource.get(ClientResponse.class);
-		
 		prepareOutput(response);
 	}
 
